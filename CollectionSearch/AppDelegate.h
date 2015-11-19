fc352ac9a68958e14a83bf78244b0e6321abe0a4
Vinoth Kannan <vinothk@syzygyenterprise.com>
2015-11-19 17:40:00 +0530

    Initial Commit

---
+++ CollectionSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionSearch
+//
+//  Created by SYZYGY01 on 19/11/15.
+//  Copyright (c) 2015 Syzygy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
